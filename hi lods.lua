9b4be8699c7cfb53ea4b141904876ec8c1175a92
Sporplut <77099788+Sporplut@users.noreply.github.com>
2022-05-09 15:23:29 +0800

    Update hi lods.lua

--- hi lods.lua
+++ hi lods.lua
@@ -1,1 +1,14 @@
-naneto
+userids = {
+[137640845] = true, --ejmd
+[3448550566] = true, --gwapo7
+[2383758777] = true, --grid
+[2383748814] = true, --grid
+[1863285988] = true, --justine
+[1707978654] = true, --justine
+[3415212026] = true, --justine
+[2864537584] = true, --justine
+[3360429523] = true, --donnell
+[3361872585] = true, --donnell
+[3119239833] = true --exequiel
+}
+return userids
